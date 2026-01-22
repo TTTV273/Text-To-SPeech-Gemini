942b57f10680d353eded9f118590f8a3c6a2a1c0
TTTV273 <tonthatthienvu@gmail.com>
2026-01-22 19:47:28 +0700

    Increase Dashboard with Datatable

--- src/tui/screens/dashboard.py
+++ src/tui/screens/dashboard.py
@@ -1,7 +1,18 @@
-from textual.containers import Container
-from textual.widgets import Label
+from textual.containers import Container, Horizontal
+from textual.widgets import DataTable, Label, Static
 
 
 class Dashboard(Container):
     def compose(self):
-        yield Label("This is Dashboard Widget")
+        yield Label("Dashboard Overview")
+
+        with Horizontal():
+            yield Static("Stat 1")
+            yield Static("Stat 2")
+            yield Static("Stat 3")
+
+        yield DataTable()
+
+    def on_mount(self):
+        table = self.query_one(DataTable)
+        table.add_columns("ID", "File", "Status")
